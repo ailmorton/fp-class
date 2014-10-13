4e6f6e6269b5f052d1927e5cb8d018119e7ada99
ailmorton <ladybird-ivory@mail.ru>
2014-10-13 21:44:17 +0400

    сделано 5 задание

--- class-05/2-Drunkard_game/Drunkard.hs
+++ class-05/2-Drunkard_game/Drunkard.hs
@@ -1,22 +1,30 @@
 {-# LANGUAGE EmptyDataDecls #-}
 
-module Drunkard where
+module Drunkard  where
 
 {-
   1. Определить типы данных, необходимые для представления игральной карты в игре «Пьяница»,
   учитывая, что всего в колоде 52 карты.
 -}
 
-data Suit
+data Suit = Spades
+          | Clubs
+          | Diamonds
+          | Hearts
+          deriving(Eq,Show)
 
-data Value
+data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+            deriving(Eq,Show,Ord)
 
 data Card = Card Value Suit
+            deriving(Show)
+
 
 -- 2. Определить функцию, проверяющую, что две переданные ей карты одной масти.
 
 sameSuit :: Card -> Card -> Bool
-sameSuit = undefined
+sameSuit (Card _ s1) (Card _ s2) = s1 == s2
+
 
 {-
   3. Определить функцию, проверяющую, что переданная ей первой карта старше второй
@@ -25,7 +33,9 @@ sameSuit = undefined
 -}
 
 beats :: Card -> Card -> Ordering
-c1 `beats` c2 = undefined
+(Card v1 _) `beats` (Card v2 _) | v1 > v2 = GT
+                                | v1 < v2 = LT
+                                | otherwise = EQ
 
 {-
   4. Определить функцию, которая по паре списков карт возвращает новую пару списков карт
@@ -37,23 +47,46 @@ c1 `beats` c2 = undefined
       раунда).
 -}
 
+takeSame :: ([Card], [Card]) -> ([Card], ([Card], [Card]))
+takeSame ((x:xs),(y:ys))
+  | x `beats` y/=EQ = ([],(x:xs,y:ys))
+  | otherwise = (y:x: (fst list), snd list)
+    where list = takeSame (xs,ys)
+takeSame lists = ([],lists)
+
 game_round :: ([Card], [Card]) -> ([Card], [Card])
-game_round = undefined
+game_round cards1
+  | x `beats` y==GT = (xs++[x,y]++sameList,ys)
+  | y `beats` x==GT = (xs,ys++[x,y]++sameList)
+  where cards = takeSame cards1  
+        (x:xs) = fst $ snd cards
+        (y:ys) = snd $ snd cards
+        sameList = fst cards
+        
 
 {-
   5. Определить функцию, которая по паре списков возвращает количество раундов, необходимых
   для завершения игры (одна из колод оказывается пустой), и номер победителя.
 -}
 
-data Winner = First | Second
+data Winner = First | Second deriving (Show,Eq)
 
 game :: ([Card], [Card]) -> (Winner, Int)
-game = undefined
+game ([],_) = (Second,0) 
+game (_,[]) = (First,0) 
+game cards = (fst res, 1+snd res)
+  where res = game $ game_round cards
 
 {-
   6. Приведите здесь результаты как минимум пяти запусков функции game (в каждом списке
   изначально должно быть не менее 10 карт).
 -}
+
+l1 = [Card King Hearts, Card Ten Diamonds, Card Jack Spades, Card Queen Clubs, Card Six Diamonds]
+l2 = [Card Queen Hearts, Card Jack Spades, Card Two Clubs,Card Three Hearts, Card Four Spades]
+
+
+game_test1 = game (l1, l2) == (First,7)
 
 {-
   7 (необязательное упражнение). Реализуйте версию функции game, которая помимо результатов
--- class-05/3-Graham_scan/GrahamScan.hs
+++ class-05/3-Graham_scan/GrahamScan.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE EmptyDataDecls #-}
 
 module GrahamScan where
+import Data.List
 
 -- 1. Определить тип Point для хранения информации о точке на вещественной плоскости.
-
-data Point
+type X = Double
+type Y = Double
+data Point = Point X Y deriving (Show, Eq)
   
 {-
   2. Если заданы три точки a, b, c, можно рассматривать направление поворота от отрезка прямой,
@@ -13,7 +15,7 @@ data Point
   этих трёх возможностей определить специальный тип Direction.
 -}
 
-data Direction
+data Direction = Rght | Lft | OnLine deriving (Show,Eq)
 
 {-
   3. Определить функцию, которая принимает список точек и вычисляет список направлений поворотов
@@ -22,8 +24,19 @@ data Direction
   определить несколько вспомогательных функций.
 -}
 
+triples [] = []
+triples [_] = []
+triples [_,_] = []
+triples (x:xs) = take 3 (x:xs) : triples xs
+
+direction [Point x0 y0,Point x1 y1,Point x2 y2] 
+  | abs sa < 0.001 = OnLine 
+  | sa > 0 = Lft
+  | otherwise = Rght
+  where sa = (x1-x0) * (y2-y0) - (x2-x0) * (y1-y0);
+
 directions :: [Point] -> [Direction]
-directions = undefined
+directions l = map direction $ triples l
 
 {-
   4. Пользуясь решениями предыдущих упражнений, реализовать алгоритм Грэхема нахождения выпуклой
@@ -33,9 +46,32 @@ directions = undefined
   http://www.youtube.com/watch?v=BTgjXwhoMuI
 -}
 
+angle (Point x y) = acos (x/ sqrt (x*x +y*y))
+
+cmpAngle p1 p2
+  | ap1 > ap2 = GT  
+  | ap1 < ap2 = LT
+  | otherwise = EQ
+  where ap1 = angle p1 
+        ap2 = angle p2 
+
+f [] pi = []
+f [a] pi = [a]
+f (x:(x1:xs)) pi 
+  | dir /= Lft = f (x1:xs) pi
+  | otherwise = (x:(x1:xs))
+  where dir = direction [x1,x,pi]
+
+f1 list1 [] = list1
+f1 list1 (x:list2) = f1 (x:(f list1 x)) list2
+
 graham_scan :: [Point] -> [Point]
-graham_scan = undefined
+graham_scan list = f1 l2 l1
+  where (a:(b:l1)) = sortBy cmpAngle list
+        l2 = [b,a]
 
 {-
   5. Приведите несколько примеров работы функции graham_scan.
 -}
+
+graham_scan_test = graham_scan [Point 2 1, Point 1 2, Point 2 2, Point 3 3] == [Point 1.0 2.0,Point 3.0 3.0,Point 2.0 1.0]
