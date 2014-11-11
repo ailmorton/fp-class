974231c0fa8c8e46fa40e9a103f6b33256f3dde8
ailmorton <ladybird-ivory@mail.ru>
2014-11-11 23:27:24 +0400

    1,2 пункты сделаны

--- class-09/02-reduce.hs
+++ class-09/02-reduce.hs
@@ -1,8 +1,7 @@
 import System.Environment
+import System.IO
+import System.Random
 
-instance Functor (Either a) where
-	fmap f (Right x) = Right (f x)
-	fmap f (Left x) = Left x
 
 {-
   Напишите функцию reduce, принимающую один целочисленный аргумент a и возвращающую 0,
@@ -22,7 +21,7 @@ reduce a
 -}
 
 reduceNF :: (Functor f, Integral a) => Int -> f a -> f a
-reduceNF n x = foldr (\z -> fmap reduce z) x [1..n]
+reduceNF n x = foldr (\a z -> fmap reduce z) x [1..n]
 
 {-
   Реализуйте следующие функции-преобразователи произвольным, но, желательно, осмысленным и
@@ -30,17 +29,33 @@ reduceNF n x = foldr (\z -> fmap reduce z) x [1..n]
 -}
 
 toList :: Integral a => [(a, a)]  -> [a]
-toList = undefined
+toList = map (\(a,b) -> a^b)  
 
 toMaybe :: Integral a => [(a, a)]  -> Maybe a
-toMaybe = undefined
+toMaybe = foldr (\(a,b) z -> if (reduce a==b) then (Just a) else z) Nothing
+
+
 
 toEither :: Integral a => [(a, a)]  -> Either String a
-toEither = undefined
+toEither l = if (f==k && (not (even k))) then Left "No even results" else Right f
+	where 
+		k = (fst (head l)) ^ (snd (head l))
+		f = foldr (\(a,b) z-> if even (a^b) then max z (a^b) else z) k l
 
 -- воспользуйтесь в этой функции случайными числами
+
+getRand gen k = head $ (randomRs (1,k) gen :: [Int])
+
+rand k = do
+  gen <- newStdGen ;
+	return $ getRand gen k
+	
+
 toIO :: Integral a => [(a, a)]  -> IO a
-toIO = undefined
+toIO l = do
+	r <- rand $ (length l) - 1
+	return $ foldr (\(a,b) z -> max (a^b) z) ((fst (head l)) ^ (snd (head l))) ( take r l)
+		
 
 {-
   В параметрах командной строки задано имя текстового файла, в каждой строке
@@ -52,15 +67,20 @@ toIO = undefined
 -}
 
 parseArgs :: [String] -> (FilePath, Int)
-parseArgs = undefined
+parseArgs l = (head l, read (head(tail l)))
+
+toPairs [] = []
+toPairs [_] = []
+toPairs l = (read (head l)::Int, read(head(tail l))::Int): toPairs (drop 2 l)
 
 readData :: FilePath -> IO [(Int, Int)]
-readData = undefined
+readData fname = do
+	content <- readFile fname
+	return $ toPairs $ words $ content
 
 main = do
   (fname, n) <- parseArgs `fmap` getArgs
   ps <- readData fname
-  undefined
   print $ reduceNF n (toEither ps)
   reduceNF n (toIO ps) >>= print
 
@@ -68,3 +88,24 @@ main = do
   Подготовьте несколько тестовых файлов, демонстрирующих особенности различных контекстов.
   Скопируйте сюда результаты вызова программы на этих файлах.
 -}
+{-
+
+*Main> :main testFile1.txt 1
+Right 0
+1953125
+
+*Main> :main testFile1.txt 5
+Right 0
+1
+
+*Main> :main testFile2.txt 6
+Left "No even results"
+0
+
+*Main> :main testFile3.txt 3
+Right 4294967296
+8931748751512963525
+
+
+-}
+
